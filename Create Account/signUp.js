e7303837c5fd6ddbd840e859900554d2f3f1a2ab
muhammad-anas16 <anas.hanf38@gmail.com>
2024-12-05 14:25:22 +0500

    sign Up Complete

--- Create Account/signUp.js
+++ Create Account/signUp.js
@@ -16,15 +16,14 @@ let passSpan = document.getElementById('span-password');
 
 // Save it in array using : ""
 
-var Data = (getItem()) ? [...getItem()] : [];
+var data = (getItem()) ? [...getItem()] : [];
 
 // function ///////////////////////////
 
-function signUpValidate(e, check) {
+function signUpValidate(e, check) { // for Validation
     if (check === 'user') {
         if (e.target.value == "") {
-            userSpan.style.visibility = "visible";
-            userSpan.innerText = "Enter User Name";
+            userSpan.style.visibility = "hidden";
         }
         else if (e.target.value.length < 3) {
             userSpan.style.visibility = "visible";
@@ -42,36 +41,43 @@ function signUpValidate(e, check) {
 
     if (check === 'password') {
         if (e.target.value == "") {
-            userSpan.style.visibility = "visible";
-            userSpan.innerText = "Enter User Name";
+            passSpan.style.visibility = "hidden";
         }
-        else if (e.target.value.length < 3) {
-            userSpan.style.visibility = "visible";
-            userSpan.innerText = "Invalid Password";
+        else if (e.target.value.length < 6) {
+            passSpan.style.visibility = "visible";
+            passSpan.innerText = "Week Password";
         }
-        else if (e.target.value.length > 3) {
-            userSpan.style.visibility = "visible";
-            userSpan.innerText = "Valid Password";
+        else if (e.target.value.length > 7) {
+            passSpan.style.visibility = "visible";
+            passSpan.innerText = "Strong Password";
         }
     }
 }
 
-function getItem() {
-    return JSON.parse(localStorage.getItem('users Data'));
-}
-
-function setItem(data) {
+function setItem(data) { // no:2
     localStorage.setItem('users Data', JSON.stringify(data));
 }
 
-function toSubmit(e) {
+function getItem() {
+    return JSON.parse(localStorage.getItem("users Data"));
+}
+
+function toSubmit(e) { // no :1
     e.preventDefault();
-    userData = [...userData, {
+
+    data = [...data,
+    {
         name: user.value,
         email: email.value,
         password: password.value,
-    }];
+    },
+    ];
 
-    setItem(userData);
+    user.value = "";
+    email.value = "";
+    password.value = "";
 
+    setItem(data); // update Local-Storage,
 };
+
+// localStorage.removeItem("users Data")
